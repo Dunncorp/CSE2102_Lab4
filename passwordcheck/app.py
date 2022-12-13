1e362a27e1a25dd4251f49633e9305546010a6c3
Dunncorp <92400666+Dunncorp@users.noreply.github.com>
2022-12-13 00:03:17 -0500

    Add files via upload

--- passwordcheck/app.py
+++ passwordcheck/app.py
@@ -1,6 +1,9 @@
 from flask import request, Flask
 import json, socket
 
+special_chars = {'[', '@', '_', '!', '#', '$', '%', '^', '&', '*', '(', ')', '<', '>', '?', '/', '\\', '|', '}', '{', '~', ':', ']'}
+numerical_chars = {'0', '1', '2', '3', '4', '5', '6', '7' , '8', '9'}
+capitals = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',  'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Z'}
 
 app = Flask(__name__)
 
@@ -14,23 +17,45 @@ def echo():
     return json.dumps(returnDictionary)
 
 #
-# curl -d "{ \"password\" : \"xxxxxxxx\" }" -X POST http://localhost:9001/check  -H "Content-type: application/json"
+# curl -d '{ "password" : "xxxxxxxx" }' -X POST http://localhost:9001/check  -H "Content-type: application/json"
 #
+
 @app.route("/check", methods=["POST"])
 def compute():
     hostName = socket.gethostname()
 
     password = request.json['password']
     password_length = len(password)
+    has_special = False
+    has_num = False
+    has_capital = False
 
     returnDictionary = {}
     returnDictionary["password"] = password
     returnDictionary["length"] = password_length
+    returnDictionary["has_special"] = has_special
+    returnDictionary["has_num"] = has_num
+    returnDictionary["has_capital"] = has_capital
+    returnDictionary["success"] = True
 
-    if password_length >= 5:
-        returnDictionary["success"] = True
-    else:
-        returnDictionary["success"] = False    
+    if password_length < 5:
+        returnDictionary["success"] = False
+    for i in range(password_length):
+    	if not has_special and password[i] in special_chars:
+    		has_special = True
+    	if not has_num and password[i] in numerical_chars:
+    		has_num = True
+    	if not has_capital and password[i] in capitals:
+    		has_capital = True
+    	if has_special and has_num and has_capital:
+    		break
+    
+    returnDictionary["has_special"] = has_special
+    returnDictionary["has_num"] = has_num
+    returnDictionary["has_capital"] = has_capital
+    
+    if not has_special or not has_num or not has_capital:
+    	returnDictionary["success"] = False
     
     return json.dumps(returnDictionary)
 
